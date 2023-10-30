34d5bd17ba4c36a47637663b06ffa48b9413adf3
craigh210 <48700844+craigh210@users.noreply.github.com>
2023-10-30 15:04:39 -0400

    Add files via upload

---
+++ dogSetterGetter/src/dogSetterGetter/Dog.java
@@ -0,0 +1,38 @@
+package dogSetterGetter;
+
+public class Dog {
+	
+	private String breed;
+	private int weight;
+	private String color;
+	
+	public Dog() {}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	
+	
+
+}
